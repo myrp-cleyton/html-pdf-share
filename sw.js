20a97ae58698f44745ecb1f39f6163072c60bb17
myrp-cleyton <cleyton.lazzarini@myrp.com.br>
2022-08-26 14:55:33 -0300

    SW GHPAges

--- sw.js
+++ sw.js
@@ -1,9 +1,55 @@
 var GHPATH = '/html-pdf-share';
-
-var VERSION = 'version_00';
-
-var URLS = [    
+var APP_PREFIX = 'gppwa_';
+var VERSION = 'version_002';
+var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/html2canvas.js`,
+  `${GHPATH}/icons/icon-48.png`,
+  `${GHPATH}/icons/icon-72.png`,
+  `${GHPATH}/icons/icon-96.png`,
+  `${GHPATH}/icons/icon-128.png`,
+  `${GHPATH}/icons/icon-144.png`,
+  `${GHPATH}/icons/icon-192.png`,
 ]
+
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i]);
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
